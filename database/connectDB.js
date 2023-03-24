436e2000a384755dfd094111e70971f34558df49
anhcoder03 <nguyenphianh161003@gmail.com>
2023-03-24 20:46:20 +0700

    update

--- database/connectDB.js
+++ database/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     await mongoose.connect(
-      `mongodb+srv://anhcoder03:Anh16101974@book.vb9opmk.mongodb.net/book-shop`,
+      `mongodb+srv://anhcoder03:Anh16101974@book.vb9opmk.mongodb.net/?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
