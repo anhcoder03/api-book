98cbae86b74d06fa837859124b57efc07b78ac1e
anhcoder03 <nguyenphianh161003@gmail.com>
2023-03-25 12:44:41 +0700

    update

--- database/connectDB.js
+++ database/connectDB.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 async function connect() {
   try {
     await mongoose.connect(
-      `mongodb+srv://anhcoder03:Anh16101974@book.vb9opmk.mongodb.net/?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@book.vb9opmk.mongodb.net/?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
